8ccb136507be1ecabea8bdf310c114ed70c90885
wanhasibala <wanhasibal1122@gmail.com>
2026-09-10 15:22:25 +0700

    fix: missing delay time for hero animation text portfolio

--- src/app/(home)/Hero.tsx
+++ src/app/(home)/Hero.tsx
@@ -189,7 +189,7 @@ const Hero = () => {
         <div className="flex flex-col w-full relative z-10 mix-blend-difference">
           <TextAnimate
             animate="slideUp"
-            delay={0.3}
+            delay={3}
             by="character"
             className="z-10 text-[14.5vw] md:text-[14vw] lg:text-[13.8vw] xl:text-[195px] 2xl:text-[216px] -mt-[16%] sm:-mt-[13%] md:-mt-[11%] lg:-mt-[9%] font-bold tracking-tight text-white mix-blend-difference select-none pointer-events-none leading-[0.85] w-full"
           >
